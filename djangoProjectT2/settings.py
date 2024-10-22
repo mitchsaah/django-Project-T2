dd6d49fb45284944a15ddabdf8e67f48e94cc8d0
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-10-22 17:27:58 +0200

    Working user authentication
    I got a note from a Youtuber that Django uses a special password policy so I tried to apply this aswell at first. Also that it was easier to extend the base.html to the login.html and register.html. But I firstly included the html in both login and register making it unresponsive. So I fixed this

--- djangoProjectT2/settings.py
+++ djangoProjectT2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c16u(7d^)7d4_!m%_e-nl)e&8s7#gq#yo_7%9qiuo^-jhwb(xs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
