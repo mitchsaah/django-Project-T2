2639d497632817a15eacd6f4c3555c6e6139c403
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-11-03 15:59:23 +0100

    Added an add_article.html and an article creation in views

--- my_app/views.py
+++ my_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Article
-
+from django import forms
 def home(request):
     return render(request, 'base.html')
 
@@ -26,3 +29,21 @@ class ArticleDetailView(DetailView):
     model = Article
     template_name = 'articles/article_detail.html'
     context_object_name = 'articles'
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'content']
+
+@login_required
+def add_article(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.save()
+            return HttpResponseRedirect(reverse('article_list'))
+    else:
+        form = ArticleForm()
+    return render(request, 'articles/add_article.html', {'form': form})
